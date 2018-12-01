c000bf4663cee7d99cc367065c463dda55ab337c
Kevin Herr <kherr@mediafly.com>
2018-12-01 07:54:09 -0600

    init project

---
+++ src/Herr.AdventOfCode/Day1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Herr.AdventOfCode
+{
+    public class Day1
+    {
+        [Fact]
+        public void Part1()
+        {
+            //var client = new HttpClient();
+        }
+    }
+}
